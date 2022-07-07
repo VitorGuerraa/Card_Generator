0bd2cfab40e2b20ddab3e2f87d429af4d4e754ed
Vitor Guerra <vitorguerra.dev@gmail.com>
2022-07-06 23:46:54 -0300

    Input Validation: input name recognizes if it is empty

--- script.js
+++ script.js
@@ -15,32 +15,57 @@ imgInput.addEventListener("change", function () {
         reader.onload = function () {
             const result = reader.result;
             img.src = result;
-
         }
         reader.readAsDataURL(file);
     }
 })
 
 
-/**----TAGS AREAS----*/
-const divTags = document.querySelector("inputTags"),
-    inputTag = tags.querySelector("input");
 
-let maxTags = 5,
-    tags = [];
+/**----INPUTS VALIDATION----*/
+const form = document.querySelector(".container"), //looking for "Container" class inside HTML document
+    nameField = form.querySelector(".inputName"), //looking for "inputName" class inside container class
+    nameVerify = nameField.querySelector(".verify"), //looking for "inputName" class inside container class
+    nameInput = nameField.querySelector("input"),
+    nameP = nameField.querySelector("p"),
 
+    localField = form.querySelector(".inputLocation"),
+    localVerify = localField.querySelector(".verify"),
+    localInput = localField.querySelector("input"),
+    localP = localField.querySelector("p"),
 
+    pronounField = form.querySelector(".inputPronoun"),
+    pronounVerify = pronounField.querySelector(".verify"),
+    pronounInput = pronounField.querySelector("input"),
+    pronounP = pronounField.querySelector("p"),
 
+    ageField = form.querySelector(".inputAge"),
+    ageVerify = ageField.querySelector(".verify"),
+    ageInput = ageField.querySelector("input"),
+    ageP = ageField.querySelector("p"),
 
+    emojiField = form.querySelector(".inputEmoji"),
+    emojiVerify = emojiField.querySelector(".verify"),
+    emojiInput = emojiField.querySelector("input");
+emojiP = emojiField.querySelector("p");
 
-/*const ul = document.querySelector("ul");
-let digts;
+form.onsubmit = (e) => {
+    e.preventDefault();//"preventDefault" Prevent an action from happening
 
-function createDigt(e) {
-    if (e.key == "Enter") {
-        digts.slice().reverse().forEach(digts => {
-            let newDigt = `<li>10</li>`;
-            ul.insertAdjacentHTML("afterbegin", newDigt);
-        });
+    if (nameInput.value == "") { //if name is empty
+        nameVerify.classList.add("verifyError");//adding a new class in this variable if this action happens
+        nameP.classList.add("pError");//adding a new class in this variable if this action happens
     }
-}*/
+
+    
+    nameInput.onkeyup = () => { //Validating value of this input
+        let patternText = /[[:alpha:]]/; //Pattern to validate name
+
+        if (!nameInput.value.match) {//if pattern not matched with value
+            nameVerify.classList.add("verifyError");
+            nameP.classList.add("pError");//adding a new class in this variable if this action happens
+        } else {,bhj            nameVerify.classList.remove("verifyError");
+            nameP.classList.remove("pError");//adding a new class in this variable if this action happens
+        }
+    }
+}
